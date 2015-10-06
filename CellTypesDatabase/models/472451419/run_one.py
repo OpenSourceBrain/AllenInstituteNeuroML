4483592c0ede3acde4e55c90a60c6f9edfbc3a9f
Padraig Gleeson <p.gleeson@gmail.com>
2015-10-06 18:34:37 +0100

    Adding a single example that saves traces in dat files

---
+++ CellTypesDatabase/models/472451419/run_one.py
@@ -0,0 +1,45 @@
+#Based on example code at: http://alleninstitute.github.io/AllenSDK/biophysical_perisomatic_script.html
+    
+from allensdk.model.biophysical_perisomatic.utils import Utils
+from allensdk.model.biophysical_perisomatic.runner import load_description
+
+    
+description = load_description('manifest.json')
+
+# configure NEURON
+utils = Utils(description)
+h = utils.h
+
+print("NEURON configured")
+
+# configure model
+manifest = description.manifest
+morphology_path = description.manifest.get_path('MORPHOLOGY')
+utils.generate_morphology(morphology_path.encode('ascii', 'ignore'))
+utils.load_cell_parameters()
+
+print("Cell loaded from: %s"%morphology_path)
+
+# configure stimulus and recording
+stimulus_path = description.manifest.get_path('stimulus_path')
+
+run_params = description.data['runs'][0]
+sweeps = [59,60]
+
+junction_potential = description.data['fitting'][0]['junction_potential']
+mV = 1.0e-3
+
+for sweep in sweeps:
+    utils.setup_iclamp(stimulus_path, sweep=sweep)
+    vec = utils.record_values()
+
+    print("Running sweep: %i"%(sweep))
+    h.finitialize()
+    h.psection()
+    h.run()
+
+    print("Finished running sweep: %i, %i data points saved"%(sweep, len(vec['v'])))
+    s_file = open('sweep_%i.v.dat'%(sweep),'w')
+    for i in range(len(vec['v'])):
+        s_file.write('%s\t%s\n'%(vec['t'][i],vec['v'][i]))
+    s_file.close()
--- CellTypesDatabase/models/download.py
+++ CellTypesDatabase/models/download.py
@@ -1,9 +1,13 @@
 from allensdk.api.queries.biophysical_perisomatic_api import \
     BiophysicalPerisomaticApi
+    
+    
+neuronal_model_ids = [472451419]
 
 bp = BiophysicalPerisomaticApi('http://api.brain-map.org')
-bp.cache_stimulus = True # change to False to not download the large stimulus NWB file
-neuronal_model_id = 472451419    # get this from the web site as above
-bp.cache_data(neuronal_model_id, working_directory='neuronal_model')
+bp.cache_stimulus = False # change to False to not download the large stimulus NWB file
+
+for neuronal_model_id in neuronal_model_ids:
+    bp.cache_data(neuronal_model_id, working_directory='%i'%neuronal_model_id)
 
 
