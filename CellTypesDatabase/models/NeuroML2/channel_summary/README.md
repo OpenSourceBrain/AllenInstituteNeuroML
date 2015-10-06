48635fadf4a45ab85ede17637283d69248fe65ad
Padraig Gleeson <p.gleeson@gmail.com>
2015-10-06 11:32:12 +0100

    Testing channels at multiple temperatures

--- CellTypesDatabase/models/NeuroML2/channel_summary/README.md
+++ CellTypesDatabase/models/NeuroML2/channel_summary/README.md
@@ -25,5 +25,23 @@ Note: mtau is an approximation from the plots
 <a href="Ca_LVA.tau.png"><img alt="Ca_LVA time course" src="Ca_LVA.tau.png" height="220"/></a>
 </td>
 </tr>
+    <tr>
+<td width="120px">
+            <sup><b>Ca_HVA</b><br/>
+            <a href="../Ca_HVA.channel.nml">Ca_HVA.channel.nml</a><br/>
+            <b>Ion: ca</b><br/>
+            <i>g = gmax * m<sup>2</sup> * h </i><br/>
+            High voltage activated Ca2+ current. 
+            
+Comment from original mod file: 
+Reuveni, Friedman, Amitai, and Gutnick, J.Neurosci. 1993</sup>
+</td>
+<td>
+<a href="Ca_HVA.inf.png"><img alt="Ca_HVA steady state" src="Ca_HVA.inf.png" height="220"/></a>
+</td>
+<td>
+<a href="Ca_HVA.tau.png"><img alt="Ca_HVA time course" src="Ca_HVA.tau.png" height="220"/></a>
+</td>
+</tr>
 </table>
 
--- CellTypesDatabase/models/NeuroML2/compare_nml2_mods.py
+++ CellTypesDatabase/models/NeuroML2/compare_nml2_mods.py
@@ -10,76 +10,79 @@ problematic = ['Im']
 
 gates = ['m', 'h']
 
-for channel_id in chans:
-    
+temperatures = [26,34]
 
-    vramp_lems_file  = '%s.rampV.lems.dat'%(channel_id)
+for temperature in temperatures:
 
-    ts = []
-    volts = []
-    for line in open(vramp_lems_file):
-        ts.append(float(line.split()[0])*1000)
-        volts.append(float(line.split()[1])*1000)
-    
-    fig = pylab.figure()
-    fig.canvas.set_window_title("Time Course(s) of activation variables of %s"%(channel_id))
+    for channel_id in chans:
 
-    pylab.xlabel('Membrane potential (mV)')
-    pylab.ylabel('Time Course - tau (ms)')
-    pylab.grid('on')
+        vramp_lems_file  = '%s.rampV.lems.%s.dat'%(channel_id, temperature)
 
-    for gate in gates:
-        
-        tau_lems_file  = '%s.%s.tau.lems.dat'%(channel_id, gate)
-        
-        if os.path.isfile(tau_lems_file):
-            taus = []
-            for line in open(tau_lems_file):
-                ts.append(float(line.split()[0])*1000)
-                taus.append(float(line.split()[1])*1000)
+        ts = []
+        volts = []
+        for line in open(vramp_lems_file):
+            ts.append(float(line.split()[0])*1000)
+            volts.append(float(line.split()[1])*1000)
 
-            pylab.plot(volts, taus, linestyle='-', linewidth=2, label="LEMS %s %s tau"%(channel_id, gate))
+        fig = pylab.figure()
+        fig.canvas.set_window_title("Time Course(s) of activation variables of %s at %sdegC"%(channel_id, temperature))
 
-            tau_mod_file  = '../NEURON/%s.%s.tau.dat'%(channel_id, gate)
-            vs = []
-            taus = []
-            for line in open(tau_mod_file):
-                vs.append(float(line.split()[0]))
-                taus.append(float(line.split()[1]))
+        pylab.xlabel('Membrane potential (mV)')
+        pylab.ylabel('Time Course - tau (ms)')
+        pylab.grid('on')
 
-            pylab.plot(vs, taus, '--x', label="Mod %s %s tau"%(channel_id, gate))
-            
-            
-    pylab.legend()
-    
-    
-    fig = pylab.figure()
-    fig.canvas.set_window_title("Steady state(s) of activation variables of %s"%(channel_id))
+        for gate in gates:
 
-    pylab.xlabel('Membrane potential (mV)')
-    pylab.ylabel('Steady state (inf)')
-    pylab.grid('on')
+            tau_lems_file  = '%s.%s.tau.lems.%s.dat'%(channel_id, gate, temperature)
 
-    for gate in gates:
-        
-        inf_lems_file  = '%s.%s.inf.lems.dat'%(channel_id, gate)
-        
-        if os.path.isfile(inf_lems_file):
-            infs = []
-            for line in open(inf_lems_file):
-                infs.append(float(line.split()[1]))
+            if os.path.isfile(tau_lems_file):
+                taus = []
+                for line in open(tau_lems_file):
+                    ts.append(float(line.split()[0])*1000)
+                    taus.append(float(line.split()[1])*1000)
 
-            pylab.plot(volts, infs, linestyle='-', linewidth=2, label="LEMS %s %s inf"%(channel_id, gate))
-            
-            inf_mod_file  = '../NEURON/%s.%s.inf.dat'%(channel_id, gate)
-            vs = []
-            infs = []
-            for line in open(inf_mod_file):
-                vs.append(float(line.split()[0]))
-                infs.append(float(line.split()[1]))
+                pylab.plot(volts, taus, linestyle='-', linewidth=2, label="LEMS %s %s tau"%(channel_id, gate))
 
-            pylab.plot(vs, infs, '--x', label="Mod %s %s inf"%(channel_id, gate))
-            
-    pylab.legend()
-    
+                tau_mod_file  = '../NEURON/%s.%s.tau.%s.dat'%(channel_id, gate, temperature)
+                vs = []
+                taus = []
+                for line in open(tau_mod_file):
+                    vs.append(float(line.split()[0]))
+                    taus.append(float(line.split()[1]))
+
+                pylab.plot(vs, taus, '--x', label="Mod %s %s tau"%(channel_id, gate))
+
+
+        pylab.legend()
+
+
+        fig = pylab.figure()
+        fig.canvas.set_window_title("Steady state(s) of activation variables of %s at %sdegC"%(channel_id, temperature))
+
+        pylab.xlabel('Membrane potential (mV)')
+        pylab.ylabel('Steady state (inf)')
+        pylab.grid('on')
+
+        for gate in gates:
+
+            inf_lems_file  = '%s.%s.inf.lems.%s.dat'%(channel_id, gate, temperature)
+
+            if os.path.isfile(inf_lems_file):
+                infs = []
+                for line in open(inf_lems_file):
+                    infs.append(float(line.split()[1]))
+
+                pylab.plot(volts, infs, linestyle='-', linewidth=2, label="LEMS %s %s inf"%(channel_id, gate))
+
+                inf_mod_file  = '../NEURON/%s.%s.inf.%s.dat'%(channel_id, gate,temperature)
+                vs = []
+                infs = []
+                for line in open(inf_mod_file):
+                    vs.append(float(line.split()[0]))
+                    infs.append(float(line.split()[1]))
+
+                pylab.plot(vs, infs, '--x', label="Mod %s %s inf"%(channel_id, gate))
+
+        pylab.legend()
+
 pylab.show()
