b6eb45ad951de3cd6df76b3fbba21134c23a0352
Padraig Gleeson <p.gleeson@gmail.com>
2015-10-08 11:38:43 +0100

    Improved export of biophys props to nml2

--- CellTypesDatabase/models/ParseAll.py
+++ CellTypesDatabase/models/ParseAll.py
@@ -6,10 +6,14 @@ from allensdk.model.biophysical_perisomatic.runner import load_description
 from pyneuroml.neuron import export_to_neuroml2
 from pyneuroml.neuron.nrn_export_utils import clear_neuron
 
+import json
+
 from pyneuroml import pynml
 
 import os
 import os.path
+
+import neuroml
 
 cell_dirs = []
 
@@ -48,7 +52,9 @@ for cell_dir in cell_dirs:
     
     nml_file_name = "%s.net.nml"%cell_dir
     nml_net_loc = "%s/%s"%(nml2_cell_dir,nml_file_name)
-    nml_cell_file = "Cell0.cell.nml"
+    nml_cell_file0 = "Cell0.cell.nml"
+    nml_cell_loc0 = "%s/%s"%(nml2_cell_dir,nml_cell_file0)
+    nml_cell_file = "%s.cell.nml"%cell_dir
     nml_cell_loc = "%s/%s"%(nml2_cell_dir,nml_cell_file)
 
 
@@ -61,7 +67,7 @@ for cell_dir in cell_dirs:
 
     print(' > Exported to: %s and %s'%(nml_net_loc, nml_cell_loc))
 
-    nml_doc = pynml.read_neuroml2_file(nml_cell_loc)
+    nml_doc = pynml.read_neuroml2_file(nml_cell_loc0)
         
     cell = nml_doc.cells[0]
         
@@ -69,5 +75,61 @@ for cell_dir in cell_dirs:
     
     for sg in cell.morphology.segment_groups:
         print("Found group: %s"%sg.id)
+        if (sg.id.startswith('ModelViewParm')) and len(sg.members)==0:
+            replace = {}
+            replace['soma_'] = 'soma'
+            replace['axon_'] = 'axon'
+            replace['apic_'] = 'apic'
+            replace['dend_'] = 'dend'
+            for prefix in replace.keys():
+                all_match = True
+                for inc in sg.includes:
+                    #print inc
+                    all_match = all_match and inc.segment_groups.startswith(prefix)
+                if all_match:
+                    print("Replacing group named %s with %s"%(sg.id,replace[prefix]))
+                    sg.id = replace[prefix]
+    
+    with open('%s_fit.json'%cell_dir, "r") as json_file:
+        cell_info = json.load(json_file)
+        
+    
+    membrane_properties = neuroml.MembraneProperties()
+    
+    for sc in cell_info['passive'][0]['cm']:
+        membrane_properties.specific_capacitances.append(neuroml.SpecificCapacitance(value='%s uF_per_cm2'%sc['cm'],
+                                            segment_groups=sc['section']))
+                                            
+    for chan in cell_info['genome']:
+        chan_name = chan['mechanism']
+        if  chan['name'] == 'g_pas':
+            chan_name = 'pas'
+        if chan['mechanism'] != 'CaDynamics':
+            cd = neuroml.ChannelDensity(id='%s_%s'%(chan_name, chan['section']),
+                                        ion_channel=chan_name,
+                                        cond_density='%s S_per_cm2'%float(chan['value']),
+                                        erev='0.0000000000000000000000000000000000mV')
+            membrane_properties.channel_densities.append(cd)
+
+    resistivities = []
+    resistivities.append(neuroml.Resistivity(value="%s ohm_cm"%cell_info['passive'][0]['ra'], segment_groups='all'))
+    
+    species = []
+    species.append(neuroml.Species(id='ca', \
+                        ion='ca',  \
+                        initial_concentration='0.0001 mM', \
+                        initial_ext_concentration='2 mM', \
+                        concentration_model="CaDynamics", \
+                        segment_groups="soma"))
+                        
+    intracellular_properties = neuroml.IntracellularProperties(resistivities=resistivities, species=species)
+
+            
+    biophysical_properties = neuroml.BiophysicalProperties(id="biophys",
+                                          intracellular_properties=intracellular_properties,
+                                          membrane_properties=membrane_properties)
+                                          
+    cell.biophysical_properties = biophysical_properties
+    
     
     pynml.write_neuroml2_file(nml_doc, nml_cell_loc)
