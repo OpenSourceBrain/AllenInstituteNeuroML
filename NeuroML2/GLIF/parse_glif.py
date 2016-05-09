a8dfc083c3db15f2296b944b1135b100702290f8
Padraig Gleeson <p.gleeson@gmail.com>
2016-05-09 16:46:26 +0100

    Restructured files

--- NeuroML2/GLIF/parse_glif.py
+++ NeuroML2/GLIF/parse_glif.py
@@ -1,77 +1,88 @@
 import sys
+import os
 import json
 
-glif_dir = sys.argv[1]
+def generate_lems(glif_dir, curr_pA, show_plot=True):
 
-
-with open('%s/model_metadata.json'%glif_dir, "r") as json_file:
-    model_metadata = json.load(json_file)
+    os.chdir(glif_dir)
     
-with open('%s/neuron_config.json'%glif_dir, "r") as json_file:
-    neuron_config = json.load(json_file)
-    
-with open('%s/ephys_sweeps.json'%glif_dir, "r") as json_file:
-    ephys_sweeps = json.load(json_file)
-    
-template_cell = '''<Lems>
+    with open('model_metadata.json', "r") as json_file:
+        model_metadata = json.load(json_file)
 
-  <%s %s/>
+    with open('neuron_config.json', "r") as json_file:
+        neuron_config = json.load(json_file)
 
-</Lems>
-'''
+    with open('ephys_sweeps.json', "r") as json_file:
+        ephys_sweeps = json.load(json_file)
 
-type = 'glifCell'
-cell_id = 'GLIF_%s'%glif_dir
-    
-attributes = ""
+    template_cell = '''<Lems>
 
-attributes +=' id="%s"'%cell_id
-attributes +='\n        C="%s F"'%neuron_config["C"]
-attributes +='\n        leakReversal="%s V"'%neuron_config["El"]
-attributes +='\n        reset="%s V"'%neuron_config["El"]
-attributes +='\n        thresh="%s V"'%( float(neuron_config["th_inf"]) * float(neuron_config["coeffs"]["th_inf"]))
-attributes +='\n        leakConductance="%s S"'%(1/float(neuron_config["R_input"]))
+      <%s %s/>
 
-file_contents = template_cell%(type, attributes)
+    </Lems>
+    '''
 
-print(file_contents)
+    type = 'glifCell'
+    cell_id = 'GLIF_%s'%glif_dir
 
-cell_file_name = '%s/%s.xml'%(glif_dir,cell_id)
-cell_file = open(cell_file_name,'w')
-cell_file.write(file_contents)
-cell_file.close()
+    attributes = ""
+
+    attributes +=' id="%s"'%cell_id
+    attributes +='\n        C="%s F"'%neuron_config["C"]
+    attributes +='\n        leakReversal="%s V"'%neuron_config["El"]
+    attributes +='\n        reset="%s V"'%neuron_config["El"]
+    attributes +='\n        thresh="%s V"'%( float(neuron_config["th_inf"]) * float(neuron_config["coeffs"]["th_inf"]))
+    attributes +='\n        leakConductance="%s S"'%(1/float(neuron_config["R_input"]))
+
+    file_contents = template_cell%(type, attributes)
+
+    print(file_contents)
+
+    cell_file_name = '%s.xml'%(cell_id)
+    cell_file = open(cell_file_name,'w')
+    cell_file.write(file_contents)
+    cell_file.close()
 
 
-import opencortex.build as oc
+    import opencortex.build as oc
 
-nml_doc, network = oc.generate_network("Test_%s"%glif_dir)
+    nml_doc, network = oc.generate_network("Test_%s"%glif_dir)
 
-pop = oc.add_single_cell_population(network,
-                                     'pop_%s'%glif_dir,
-                                     cell_id)
-                                     
-                                     
-pg = oc.add_pulse_generator(nml_doc,
-                       id="pg0",
-                       delay="100ms",
-                       duration="1000ms",
-                       amplitude="150 pA")
+    pop = oc.add_single_cell_population(network,
+                                         'pop_%s'%glif_dir,
+                                         cell_id)
 
-                                   
-oc.add_inputs_to_population(network,
-                            "Stim0",
-                            pop,
-                            pg.id,
-                            all_cells=True)
+
+    pg = oc.add_pulse_generator(nml_doc,
+                           id="pg0",
+                           delay="100ms",
+                           duration="1000ms",
+                           amplitude="%s pA"%curr_pA)
+
+
+    oc.add_inputs_to_population(network,
+                                "Stim0",
+                                pop,
+                                pg.id,
+                                all_cells=True)
+
+
+
+    nml_file_name = '%s.net.nml'%network.id
+    oc.save_network(nml_doc, nml_file_name, validate=True)
+
+    oc.generate_lems_simulation(nml_doc, 
+                                network, 
+                                nml_file_name, 
+                                include_extra_files = [cell_file_name,'../GLIFs.xml'],
+                                duration =      1200, 
+                                dt =            0.01)
+
+    os.chdir('..')
                             
-                            
-
-nml_file_name = '%s.net.nml'%network.id
-oc.save_network(nml_doc, nml_file_name, validate=True)
-
-oc.generate_lems_simulation(nml_doc, 
-                            network, 
-                            nml_file_name, 
-                            include_extra_files = [cell_file_name,'GLIFs.xml'],
-                            duration =      1200, 
-                            dt =            0.05)
+if __name__ == '__main__':
+    
+    glif_dir = sys.argv[1]
+    curr_pA = float(sys.argv[2])
+    show_plot = '-nogui' not in sys.argv
+    generate_lems(glif_dir, curr_pA, show_plot=show_plot)
--- NeuroML2/GLIF/run_glif.py
+++ NeuroML2/GLIF/run_glif.py
@@ -2,38 +2,73 @@ import allensdk.core.json_utilities as json_utilities
 from allensdk.model.glif.glif_neuron import GlifNeuron
 import sys
 
-glif_dir = sys.argv[1]
-
 from pyneuroml import pynml
 
-# initialize the neuron
-neuron_config = json_utilities.read('%s/neuron_config.json'%glif_dir)
-neuron = GlifNeuron.from_dict(neuron_config)
+def run_one_cell(glif_dir, curr_pA, show_plot=True):
 
-# important! set the neuron's dt value for your stimulus in seconds
-neuron.dt = 5e-6
+    # initialize the neuron
+    neuron_config = json_utilities.read('%s/neuron_config.json'%glif_dir)
+    neuron = GlifNeuron.from_dict(neuron_config)
 
-# make a short square pulse. stimulus units should be in Amps.
-stimulus = [ 0.0 ] * int(0.1/neuron.dt) + [ 150e-12 ] * int(1/neuron.dt) + [ 0.0 ] * int(0.1/neuron.dt)
-times = [ i*neuron.dt for i in range(len(stimulus)) ]
+    # important! set the neuron's dt value for your stimulus in seconds
+    neuron.dt = 5e-7
 
-# simulate the neuron
-output = neuron.run(stimulus)
+    # make a short square pulse. stimulus units should be in Amps.
+    stimulus = [ 0.0 ] * int(0.1/neuron.dt) + [ curr_pA * 1e-12 ] * int(1/neuron.dt) + [ 0.0 ] * int(0.1/neuron.dt)
+    times = [ i*neuron.dt for i in range(len(stimulus)) ]
 
-print "run.."
+    # simulate the neuron
+    output = neuron.run(stimulus)
 
-voltage = output['voltage']
-threshold = output['threshold']
-spike_times = output['interpolated_spike_times']
+    print "run.."
 
-print spike_times
+    voltage = output['voltage']
+    threshold = output['threshold']
+    spike_times = output['interpolated_spike_times']
+
+    print spike_times
+
+    info = "Model %s; %spA stimulation; %i spikes"%(glif_dir,curr_pA,len(spike_times))
+    print(info)
+    
+    v_file = open('%s/original.v.dat'%glif_dir,'w')
+    th_file = open('%s/original.thresh.dat'%glif_dir,'w')
+    
+    for i in range(len(times)):
+        t = times[i]
+        v = voltage[i]
+        th = threshold[i]
+        v_file.write('%s\t%s\n'%(t,v))
+        th_file.write('%s\t%s\n'%(t,th))
+    v_file.close()
+    th_file.close()
+    
+    pynml.generate_plot([times],
+                            [voltage], 
+                            "Membrane potential; %s"%info, 
+                            colors = ['k'], 
+                            xaxis = "Time (s)", 
+                            yaxis = "Voltage (V)", 
+                            grid = True,
+                            show_plot_already=False,
+                            save_figure_to='%s/MembranePotential_%ipA.png'%(glif_dir,curr_pA))
+                
+    pynml.generate_plot([times],
+                            [threshold], 
+                            "Threshold; %s"%info, 
+                            colors = ['r'], 
+                            xaxis = "Time (s)", 
+                            yaxis = "Voltage (V)", 
+                            grid = True,
+                            show_plot_already=show_plot,
+                            save_figure_to='%s/Threshold_%ipA.png'%(glif_dir,curr_pA))
 
 
-pynml.generate_plot([times],
-                        [voltage], 
-                        "vv", 
-                        colors = ['k'], 
-                        xaxis = "Time (s)", 
-                        yaxis = "Voltage (mV)", 
-                        grid = True,
-                        show_plot_already=True)
+
+if __name__ == '__main__':
+    
+    glif_dir = sys.argv[1]
+    curr_pA = float(sys.argv[2])
+    show_plot = '-nogui' not in sys.argv
+    run_one_cell(glif_dir, curr_pA, show_plot=show_plot)
+    
